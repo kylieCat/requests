a7d2b3bb5f4d3dc30efe735c2a84a33ba004b9eb
Kylie <imauld@gmail.com>
2018-08-06 08:16:49 -0700

    Split up large file

---
+++ request.go
@@ -0,0 +1,74 @@
+package requests
+
+import (
+	"net/url"
+	"io"
+	"bytes"
+)
+
+type RequestOptionFunc func(*Request) error
+
+// WithHeader applies the header to the request.
+func WithHeader(key, value string) RequestOptionFunc {
+	return func(r *Request) error {
+		r.Headers = append(r.Headers, Header{
+			Key:    key,
+			Values: []string{value},
+		})
+		return nil
+	}
+}
+
+// WithQueryParam adds a key, value pair to the `QueryParam` struct.
+func WithQueryParam(key string, values ...string) RequestOptionFunc {
+	return func(r *Request) error {
+		r.QueryParams[key] = append(r.QueryParams[key], values...)
+		return nil
+	}
+}
+
+// WithQueryParams sets the `QueryParams` field on the `Request` struct.
+func WithQueryParams(params url.Values) RequestOptionFunc {
+	return func(r *Request) error {
+		r.QueryParams = params
+		return nil
+	}
+}
+
+// WithBasicAuth attaches an Authorization header to the `Request` struct that
+// uses basic auth.
+func WithBasicAuth(token string) RequestOptionFunc {
+	return func(request *Request) error {
+		request.Headers = append(request.Headers, Header{Key:"Authorization",  Values: []string{"Basic " + token}})
+		return nil
+	}
+}
+
+// Request is a HTTP request.
+type Request struct {
+	Method  string
+	URL     string
+	Headers []Header
+	Body    io.Reader
+	// QueryParams is a map of query parameters. Uses url.Values
+	QueryParams url.Values
+	// Fragment is the bit after a #
+	Fragment string
+	// User info
+	User string
+}
+
+// GetUrl constructs the final URL from the components on the `Request` struct.
+func (r Request) GetUrl() string {
+	var buf bytes.Buffer
+	buf.WriteString(r.URL)
+	if r.QueryParams != nil {
+		buf.WriteByte('?')
+		buf.WriteString(r.QueryParams.Encode())
+	}
+	if r.Fragment != "" {
+		buf.WriteByte('#')
+		buf.WriteString(r.Fragment)
+	}
+	return buf.String()
+}
---
+++ response.go
@@ -0,0 +1,72 @@
+package requests
+
+import (
+	"strings"
+	"io"
+	"encoding/json"
+	"io/ioutil"
+)
+
+// Response is a HTTP response.
+type Response struct {
+	*Request
+	Status
+	Headers []Header
+	Body
+}
+
+// Header returns the canonicalised version of a response header as a string
+// If there is no key present in the response the empty string is returned.
+// If multiple headers are present, they are canonicalised into as single string
+// by joining them with a comma. See RFC 2616 § 4.2.
+func (r *Response) Header(key string) string {
+	var vals []string
+	for _, h := range r.Headers {
+
+		// TODO(dfc) § 4.2 states that not all header values can be combined, but equally those
+		// that cannot be combined with a comma may not be present more than once in a
+		// header block.
+		if h.Key == key {
+			vals = append(vals, h.Values...)
+		}
+	}
+	return strings.Join(vals, ",")
+}
+
+type Body struct {
+	io.ReadCloser
+	json *json.Decoder
+	content string
+}
+
+// JSON decodes the next JSON encoded object in the body to v.
+func (b *Body) JSON(v interface{}) error {
+	if b.json == nil {
+		b.json = json.NewDecoder(b)
+	}
+	return b.json.Decode(v)
+}
+
+// Content returns the bosy of the response in it's raw sting format
+func (b *Body) Content() string {
+	if b.content == "" {
+		content, err := ioutil.ReadAll(b)
+		if err != nil {
+			return ""
+		}
+		b.content = string(content)
+	}
+	return b.content
+}
+
+// return the body as a string, or bytes, or something
+func headers(h map[string][]string) []Header {
+	headers := make([]Header, 0, len(h))
+	for k, v := range h {
+		headers = append(headers, Header{
+			Key:    k,
+			Values: v,
+		})
+	}
+	return headers
+}
